532dd6709b6b6b8b529f729c5686c7bcb4656dd9
Eun Ji <ghkdghtnr90@naver.com>
2023-06-01 23:04:53 +0900

    implement hw5

--- figure_main_2.py
+++ figure_main_2.py
@@ -5,13 +5,13 @@ area_rectangle, area_right_triangle, area_ellipse 함수들에서
 예외 발생 시 0이하의 값은 입력할 수 없습니다를 출력하도록 변경!
 """
 
-import figure
+import figure_2
 
-myline = figure.line(10, 20)
+myline = figure_2.line(10, 20)
 
 width, height = myline.get_length()
 try:
-    rectangle = figure.area_rectangle(width, height)
+    rectangle = figure_2.area_rectangle(width, height)
     print(rectangle)
 except ValueError:
     print("please input positive number for width and height")
@@ -19,7 +19,7 @@ except ValueError:
 myline.set_length(20, 30)
 width, height = myline.get_length()
 try:
-    triangle = figure.area_right_triangle(width, height)
+    triangle = figure_2.area_right_triangle(width, height)
     print(triangle)
 except ValueError:
     print("please input positive number for width and height")
@@ -27,7 +27,9 @@ except ValueError:
 myline.set_length(30, 40)
 width, height = myline.get_length()
 try:
-    ellipse = figure.area_ellipse(width, height)
+    ellipse = figure_2.area_ellipse(width, height)
     print(ellipse)
 except ValueError:
     print("please input positive number for width and height")
+
+# %%
